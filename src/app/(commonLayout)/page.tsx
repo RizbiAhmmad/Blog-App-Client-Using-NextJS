10ed7262dbef22a62d0945d0ab3cdaf354da86a9
Rizbi ahmmad <contact.rizbi123@gmail.com>
2026-01-25 01:36:05 +0600

    Securing routes by their role, showing blogs by card, amd implementing search options

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,13 +1,29 @@
-import { Button } from "@/components/ui/button";
-import { userService } from "@/services/user.service";
+import BlogCard from "@/components/modules/homepage/BlogCard";
+import { blogService } from "@/services/blog.service";
+import { BlogPost } from "@/types";
+// import { userService } from "@/services/user.service";
 
 export default async function Home() {
-  const { data } = await userService.getSession();
+  // const { data } = await userService.getSession();
+  // console.log(data);
+
+  const { data } = await blogService.getBlogPosts(
+    {
+      isFeatured: false,
+      // search:""
+    },
+    {
+      cache: "no-store",
+      // revalidate:10
+    },
+  );
   console.log(data);
 
   return (
-    <div>
-      <Button variant="outline">Button</Button>
+    <div className="grid grid-cols-3 max-w-7xl mx-auto px-4 gap-5">
+      {data?.data?.map((post: BlogPost) => (
+        <BlogCard key={post.id} post={post} />
+      ))}
     </div>
   );
 }
